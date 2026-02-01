12d5621c464d38f8a31f674fa96ff6f35a0c3926
AnthonyTranCode <66093871+AnthonyTranCode@users.noreply.github.com>
2026-02-01 14:09:29 -0800

    Update to code

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/clonehabit/', // GitHub Pages repo name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
